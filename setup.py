c1334eef4490f6314503afa3159e4176687c3455
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-01-18 18:00:03 +0000

    Fix dtoolcore version dependency

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     download_url="{}/tarball/{}".format(url, version),
     install_requires=[
         "click",
-        "dtoolcore>=2.2.0",
+        "dtoolcore>=3.0.0",
         "dtool_cli",
         "boto3",
     ],

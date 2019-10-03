94404a2f48c6b7efe21585658f465b31929ac166
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2019-10-03 17:40:31 +0100

    Use datasets rather then items on gantt y-axsis

--- parse_logs/create_gantt_plot.R
+++ parse_logs/create_gantt_plot.R
@@ -8,14 +8,15 @@ data$num_retries <- as.factor(data$num_retries)
 data$upload_start <- as.POSIXct(data$upload_start, origin="1970-01-01", tz="GMT")
 data$upload_end <- as.POSIXct(data$upload_end, origin="1970-01-01", tz="GMT")
 data$size_in_gib <- data$size_in_bytes / (1024 * 1024 * 1024)
+data$ds_name <- as.factor(data$ds_name)
 
-g <- ggplot(data=data, aes(x=upload_start, xend=upload_end, y=ID, yend=ID, size=size_in_gib, colour=num_retries))
+g <- ggplot(data=data, aes(x=upload_start, xend=upload_end, y=ds_name, yend=ds_name, size=size_in_gib, colour=num_retries))
 g <- g + geom_segment()
 g <- g + scale_x_datetime(labels=date_format("%H:%M:%S"))
 g <- g + labs(
     title="ECS upload performance",
     x="Time",
-    y="Item ID",
+    y="Datasets",
     size="Size (GiB)",
     colour="Retries"
 )

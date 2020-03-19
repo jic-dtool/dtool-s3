f85d608ac8d13a65d9b3dfcb947621d1341e42a4
Tjelvar Olsson <olssont@nbi.ac.uk>
2020-03-19 16:31:09 +0000

    Update dtoolcore requirement

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     download_url="{}/tarball/{}".format(url, version),
     install_requires=[
         "click",
-        "dtoolcore>=3.13",
+        "dtoolcore>=3.17",
         "dtool_cli",
         "boto3",
     ],
